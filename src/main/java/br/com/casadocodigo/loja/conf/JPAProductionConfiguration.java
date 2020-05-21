524537ac4c33e4c73eba406785fa55ac3415bfdb
Leonardo Fuzeto <fuzetolhf@gmail.com>
2020-05-20 21:06:56 -0300

    configuracoes para deploy em produção

--- src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/JPAProductionConfiguration.java
@@ -11,9 +11,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.sql.DataSource;
-
-
+@EnableTransactionManagement
 @Profile("prod")
 public class JPAProductionConfiguration {
 
@@ -22,23 +20,21 @@ public class JPAProductionConfiguration {
 
     @Bean
     public Properties additionalProperties() {
-        Properties props = new Properties();
-        props.setProperty("hibernate.dialect",
-                "org.hibernate.dialect.PostgreSQLDialect");
-        props.setProperty("hibernate.show_sql", "true");
-        props.setProperty("hibernate.hbm2ddl.auto", "update");
-        return props;
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty("hibernate.show_sql", "true");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+        return properties;
     }
 
     @Bean
-    public DataSource dataSource() throws URISyntaxException {
-        DriverManagerDataSource dataSource =
-                new DriverManagerDataSource();
+    private DriverManagerDataSource dataSource() throws URISyntaxException {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
 
         URI dbUrl = new URI(environment.getProperty("DATABASE_URL"));
-        dataSource.setUrl("jdbc:postgresql://" + dbUrl.getHost() +
-                ":" + dbUrl.getPort() + dbUrl.getPath());
+
+        dataSource.setUrl("jdbc:postgresql://"+dbUrl.getHost()+":"+dbUrl.getPort()+dbUrl.getPath());
         dataSource.setUsername(dbUrl.getUserInfo().split(":")[0]);
         dataSource.setPassword(dbUrl.getUserInfo().split(":")[1]);
 
